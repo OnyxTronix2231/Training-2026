df87dcaf18fbfefb52aadcc4dd70823126f1a60e
yoav2164 <bashanyvy@gmail.com>
2026-07-28 16:07:09 +0300

    Create bigOrSmall.java

---
+++ src/main/java/L2/training/bigOrSmall.java
@@ -0,0 +1,15 @@
+package L2.training;
+
+public class bigOrSmall {
+    public static void check(int num) {
+        if(num > 5) {
+            System.out.println("big");
+        } else {
+            System.out.println("small");
+        }
+    }
+
+    public static void main(String[] args) {
+        check(12);
+    }
+}
